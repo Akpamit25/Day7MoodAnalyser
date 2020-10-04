6564ab3c3b111f4f307e5efc1f3a2b29de26800c
Amit Kumar <amitnitsri25@gmail.com>
2020-10-05 00:39:41 +0530

    Testcase

--- src/main/java/com/day7moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/day7moodanalyzer/MoodAnalyzer.java
@@ -6,6 +6,8 @@ public class MoodAnalyzer {
 	public String analyzeMood(String message) {
 		if(message.toLowerCase().contains("sad"))
 			return "SAD";			
+		else if(message.toLowerCase().contains("happy"))
+			return "HAPPY";
 		else
 			return "HAPPY";
 	}
